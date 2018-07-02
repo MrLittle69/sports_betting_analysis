5fd8c63b85fc18f4e949c330662dbc66ffd298db
Oli Cairns <olicairns93@gmail.com>
2018-07-02 21:14:06 +0100

    Tweaks to glitko@

--- Scripts/2. Analysis/4. Tennis analysis - match level Glitko.py
+++ Scripts/2. Analysis/4. Tennis analysis - match level Glitko.py
@@ -7,12 +7,124 @@ Created on Sat Apr 28 19:20:10 2018
 import pandas as pd
 import seaborn
 import math
-import elo_functions as elo
 from matplotlib import pyplot as plt
 from scipy import optimize
 import numpy as np
+import elo
+"""
+Source: http://glicko.net/glicko/glicko2.pdf
 
-tau = 0.6
+Step 1: 
+
+    Determine a rating and RD for each player at the onset of the rating period. The
+    system constant, τ , which constrains the change in volatility over time, needs to be
+    set prior to application of the system. Reasonable choices are between 0.3 and 1.2,
+    though the system should be tested to decide which value results in greatest predictive
+    accuracy. Smaller values of τ prevent the volatility measures from changing by large
+    amounts, which in turn prevent enormous changes in ratings based on very improbable
+    results. If the application of Glicko-2 is expected to involve extremely improbable
+    collections of game outcomes, then τ should be set to a small value, even as small as,
+    say, τ = 0.2.
+    (a) If the player is unrated, set the rating to 1500 and the RD to 350. Set the player’s
+    volatility to 0.06 (this value depends on the particular application).
+    (b) Otherwise, use the player’s most recent rating, RD, and volatility σ.
+    
+    DONE - Initialising Gliko2 scaling to avoid steps 2 and 8.
+
+Step 2. 
+
+    For each player, convert the ratings and RD’s onto the Glicko-2 scale:
+    µ = (r − 1500)/173.7178
+    φ = RD/173.7178
+    
+    REDUNDANT - initialising with values 0 and 350/173.7178
+    
+    The value of σ, the volatility, does not change.
+    We now want to update the rating of a player with (Glicko-2) rating µ, rating deviation
+    φ, and volatility σ. He plays against m opponents with ratings µ1, . . . , µm, rating
+    deviations φ1, . . . , φm. Let s1, . . . , sm be the scores against each opponent (0 for a loss,
+    0.5 for a draw, and 1 for a win). The opponents’ volatilities are not relevant in the
+    calculations.
+    
+    
+
+Step 3. 
+
+    Compute the quantity v. This is the estimated variance of the team’s/player’s
+    rating based only on game outcomes.
+
+    i)
+    v = (Sum j g(φj )^2E(µ, µj, φj ){1 − E(µ, µj, φj )})^-1
+    
+    ii)
+    g(φ) = 1 / 1 + 3φ2/π2
+    
+    iii)
+    E(µ, µj, φj ) = 1 / 1 + exp(−g(φj )(µ − µj )).
+
+
+Step 4. 
+
+    Compute the quantity ∆, the estimated improvement in rating by comparing the
+    pre-period rating to the performance rating based only on game outcomes.
+    ∆ = v*sum( g(φj ){sj − E(µ, µj, φj )})
+    with g() and E() defined above.
+
+Step 5. 
+
+    Determine the new value, σ' of the volatility. This computation requires iteration.
+    
+    2
+    1. Let a = ln(σ2), and define
+    f(x) = e^x(∆2 − φ 2 − v − ex) /2(φ2 + v + ex^)2 −(x − a)/τ2
+    
+    Also, define a convergence tolerance, ε. The value ε = 0.000001 is a sufficiently
+    small choice.
+    
+    2. Set the initial values of the iterative algorithm.
+    • Set A = a = ln(σ2)
+    • If ∆2 > φ2 + v, then set B = ln(∆2 − φ2 − v).
+    If ∆2 ≤ φ2 + v, then perform the following iteration:
+    (i) Let k = 1
+    (ii) If f(a − kτ ) < 0, then
+    Set k ← k + 1
+    Go to (ii).
+    and set B = a − kτ . The values A and B are chosen to bracket ln(σI2), and
+    the remainder of the algorithm iteratively narrows this bracket.
+    
+    3. Let fA = f(A) and fB = f(B).
+    
+    4. While |B − A| > ε, carry out the following steps.
+    (a) Let C = A + (A − B)fA/(fB − fA), and let fC = f(C).
+    (b) If fCfB < 0, then set A ← B and fA ← fB; otherwise, just set fA ← fA/2.
+    (c) Set B ← C and fB ← fC.
+    (d) Stop if |B − A| ≤ ε. Repeat the above three steps otherwise.
+    5. Once |B − A| ≤ ε, set
+    σ' ← eA/2
+
+
+Step 6. 
+
+    Update the rating deviation to the new pre-rating period value, φ'
+    φ' = root(φ2 + σ2)
+
+Step 7. 
+
+    Update the rating and RD to the new values, µ' and φ':
+    
+    φ' = 1/root(1/φ'2 + 1/v)
+
+    µ' = µ + φ'2 sum(g(φj ){sj − E(µ, µj, φj )}
+
+Step 8 
+
+    Redundant (using Gliko 2 scales)
+                     
+"""
+
+TAU = 0.6
+EPSILON = 0.000001
+
 
 def g(RD):
     return 1.0/math.sqrt((1+(3*RD**2))/math.pi**2)
@@ -28,22 +140,51 @@ def v_and_delta(player_matches,player_name,ratings_dict):
     for match_j in player_matches:
         rating_j = ratings_dict[player_matches['rival_name']]['rating']
         RD_j = ratings_dict[player_matches['rival_name']]['RD']
-        outcome_j = player_matches['outcome']
+        s_j = player_matches['outcome']
         E_j =E(rating,rating_j,RD_j)
         g_j = g(RD_j)
-        v_total +=  E_j*(1-E_j)*(g_j**2)
-        delta_total + g_j*(outcome_j- E_j)
+        v_total +=  (g_j**2)*E_j*(1-E_j)
+        delta_total += g_j*(s_j- E_j)
         
     return 1.0/v_total, delta_total
 
-def f(x,delta_i,RD_i,v_i,tao,a):
+def f(x,delta_i,RD_i,v_i,TAU,a):
     e_x = math.exp(x)
     top_a = (e_x*(delta_i**2-RD_i**2-v_i-e_x))
     bottom_a = 2*((RD_i**2+v_i+e_x)**2)
-    return top_a/bottom_a + (x-a)/(tao**2)
+    return top_a/bottom_a + (x-a)/(TAU**2)
 
-def update_vol_i():
-    a = math.log(RD_i**2)
+def update_vol_i(player_matches,player_name,ratings_dict):
+    vol_i = ratings_dict[player_name]['vol']
+    RD_i = ratings_dict[player_name]['RD']
+    A = math.log(vol_i**2)
+    v_i, delta_i = v_and_delta(player_matches,player_name,ratings_dict)
+    if vol_i**2 > RD_i**2 + v_i:
+        B = math.log(vol_i**2 - RD_i**2 - v_i)
+    else:
+        k = 1
+        while f(A - k*TAU) < 0:
+            k +=1
+        B = A - k*TAU
+    Fa = f(A)
+    Fb = f(B)
+    while abs(B - A) > EPSILON:
+        C = A + ((A-B)*Fa)/ (Fb - Fa)
+        Fc = f(C)
+        if Fc*Fb < 0:
+            A = B
+            Fa = Fb
+        else:
+            Fa /= 2
+        B = C
+        Fb = Fc
+    return math.exp(A/2)
+        
+def update_rd_i(RD_i,sigma_i):
+    return math.sqrt(RD_i**2,sigma_i**2)
+    
+
+            
 
 
 #Solve recursive formula for base case, designed to be applied rowwise to pandas df
@@ -102,9 +243,10 @@ unique_players = set(elo_df['Player 1']).union(set(elo_df['Player 2']))
 
 vol_change = 0.75
 
-ratings_dict = {}
+ratings_df = pd.DataFrame(columns = ['player','rating','RD','vol'])
+
 for player in unique_players:
-    ratings_dict[player]={'rating':0,'RD':350/173.7178,'vol':0.06}
+    ratings_df.append({'player':player,'rating':0,'RD':350/173.7178,'vol':0.06})
     
 uniq_months=  elo_df['Year-Month'].unique()
 
